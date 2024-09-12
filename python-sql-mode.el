45b792da6f619268f57e0abae959be7ad92fa9d1
Taro Sato <okomestudio@gmail.com>
2024-09-12 13:57:00 -0700

    Update docstring

--- python-sql-mode.el
+++ python-sql-mode.el
@@ -1,7 +1,35 @@
 ;;; python-sql-mode.el --- python-sql-mode  -*- lexical-binding: t -*-
+;;
+;; Copyright (C) 2024 Taro Sato
+;;
+;; Author: Taro Sato <okomestudio@gmail.com>
+;; URL: https://github.com/okomestudio/python-sql-mode.el
+;; Version: 0.2
+;; Keywords:
+;; Package-Requires: ((emacs "29.1"))
+;;
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
 ;;
 ;; Python-SQL polymode.
+;;
+;; This mode interprets in-line code in string as SQL when it is (1)
+;; triple quoted (""") and (2) either the variable starts with the
+;; prefix "sql" or the string starts with the SQL comment "-- sql".
 ;;
 ;;; Code:
 
